a0cff187c19b18ec07f0f1a074d0ba711aa38b43
Rachit-3850 <2020ucp1736@mnit.ac.in>
2021-08-08 22:00:59 +0530

    Day29 Completed

---
+++ Day29/weather/src/store.js
@@ -0,0 +1,5 @@
+import rootreducer from "./reducers";
+import { createStore ,applyMiddleware } from "redux";
+import thunk from "redux-thunk"
+const store = createStore(rootreducer , applyMiddleware(thunk))
+export default store
